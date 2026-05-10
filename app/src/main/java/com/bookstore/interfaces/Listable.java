4bedc8ea5578ba91bc14e2cad38afdb82c46f228
Luke-Mthwazi <lukhanyilemthwazi@gmail.com>
2026-05-10 02:59:13 -0300

    Add interfaces, abstract class and Textbook model - Luke-Mthwazi

--- app/src/main/java/com/bookstore/interfaces/Listable.java
+++ app/src/main/java/com/bookstore/interfaces/Listable.java
@@ -1,5 +1,6 @@
 package com.bookstore.interfaces;
-
+// Listable Interface - Implemented by Luke-Mthwazi
+// Defines the contract for listing items in the marketplace
 public interface Listable {
     boolean validate();
     void listItem();
